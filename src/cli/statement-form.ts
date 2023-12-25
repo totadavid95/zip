c77d17ab2f7214b2ff531548d86d7ffdfd089afc
totadavid95 <totadavid95@gmail.com>
2023-12-25 14:54:11 +0100

    Statement form

---
+++ src/cli/statement-form.ts
@@ -0,0 +1,94 @@
+/**
+ * @file Interactive statement form.
+ */
+
+import { i18next } from '../i18n';
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+import { RE_NEPTUN, RE_NL, SPACE } from '../common/constants';
+
+interface StatementFormData {
+    name: string;
+    neptun: string;
+}
+
+/**
+ * Interactive statement form.
+ *
+ * @returns Statement form data.
+ */
+export const statementForm = async (): Promise<StatementFormData> => {
+    // Show statement preview
+    const statement = i18next.t('statement');
+    statement.split(RE_NL).forEach((line) => console.log(chalk.gray(line)));
+
+    // Ask for confirmation
+    const confirmation = await inquirer.prompt([
+        {
+            type: 'list',
+            name: 'confirmation',
+            message: i18next.t('statementConfirmation'),
+            choices: [
+                { name: i18next.t('statementConfirmationAccept'), value: true },
+                { name: i18next.t('statementConfirmationDecline'), value: false },
+            ],
+        },
+    ]);
+
+    if (!confirmation.confirmation) {
+        throw new Error(i18next.t('statementConfirmationDeclined'));
+    }
+
+    console.log(chalk.green(i18next.t('statementConfirmationAccepted')));
+
+    // Ask for student name and neptun code
+    const questions = [
+        {
+            type: 'input',
+            name: 'name',
+            message: i18next.t('statementNameQuestion'),
+            validate(name: string) {
+                name = name.trim();
+                if (name.length < 2) {
+                    return i18next.t('statementNameTooShort');
+                }
+                if (name.indexOf(SPACE) === -1) {
+                    return i18next.t('statementNameMissingSpace');
+                }
+                return true;
+            },
+            filter(name: string) {
+                return name
+                    .split(SPACE)
+                    .filter((part) => part.length)
+                    .map((part) => {
+                        const partLower = part.toLowerCase();
+                        return partLower.charAt(0).toUpperCase() + partLower.slice(1);
+                    })
+                    .join(SPACE);
+            },
+        },
+        {
+            type: 'input',
+            name: 'neptun',
+            message: i18next.t('statementNeptunQuestion'),
+            validate(neptun: string) {
+                neptun = neptun.trim();
+                if (neptun.length !== 6) {
+                    return i18next.t('statementNeptunLength');
+                }
+                if (!neptun.match(RE_NEPTUN)) {
+                    return i18next.t('statementNeptunInvalid');
+                }
+                return true;
+            },
+            filter(neptun: string) {
+                return neptun.toUpperCase();
+            },
+        },
+    ];
+
+    return await inquirer.prompt(questions);
+};
+
+statementForm();
--- src/statement/statement-form.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @file Statement management
- */
-
-const statementFill = () => {};
